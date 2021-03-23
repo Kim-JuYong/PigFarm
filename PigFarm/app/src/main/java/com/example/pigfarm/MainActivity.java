543a15dfdbcce7ecff74653d9f2150146582b9da
jyoung <juyong4190@daum.net>
2021-03-23 14:23:46 +0900

    main + bottomNavigation

---
+++ PigFarm/app/src/main/java/com/example/pigfarm/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.pigfarm;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView; // 바텀네비게이션 뷰
+    private FragmentManager manager = getSupportFragmentManager();
+    private FragmentTransaction transaction;
+
+    private HomeFragment homeFragment = new HomeFragment();
+    private AnalyzeFragment analyzeFragment = new AnalyzeFragment();
+    private RecommendFragment recommendFragment = new RecommendFragment();
+    private MypageFragment mypageFragment = new MypageFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, homeFragment).commitAllowingStateLoss();
+        //바텀 네비게이션뷰 안의 아이템들 설정
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentTransaction transaction = manager.beginTransaction();
+                switch (item.getItemId()) {
+                    case R.id.home: {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, homeFragment).commitAllowingStateLoss();
+                        break;
+                    }
+                    case R.id.analyze: {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, analyzeFragment).commitAllowingStateLoss();
+                        break;
+                    }
+                    case R.id.recommend: {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, recommendFragment).commitAllowingStateLoss();
+                        break;
+                    }
+                    case R.id.mypage: {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, mypageFragment).commitAllowingStateLoss();
+                        break;
+                    }
+                }
+                return true;
+            }
+        });
+    }
+}

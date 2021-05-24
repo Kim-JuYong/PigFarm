eb667aab4d635c0ec83c6cd5b0630780d5ca5419
jyoung <juyong4190@daum.net>
2021-05-24 16:05:18 +0900

    acitivty 에 카메라 구현

--- PigFarm/app/src/main/java/com/example/pigfarm/HomeFragment.java
+++ PigFarm/app/src/main/java/com/example/pigfarm/HomeFragment.java
@@ -1,18 +1,43 @@
 package com.example.pigfarm;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
+
+import java.io.File;
 
 public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.home_fragment, container, false);
+        View v = inflater.inflate(R.layout.home_fragment, container, false);
+        return v;
+
     }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.meal_photo_register_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((PhotoActivity) getActivity()).dispatchTakePictureIntent();
+            }
+        });
+    }
+
 }
--- PigFarm/app/src/main/java/com/example/pigfarm/MainActivity.java
+++ PigFarm/app/src/main/java/com/example/pigfarm/MainActivity.java
@@ -1,16 +1,31 @@
 package com.example.pigfarm;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
+    //ImageView imageView;
 
     private BottomNavigationView bottomNavigationView; // 바텀네비게이션 뷰
     private FragmentManager manager = getSupportFragmentManager();
@@ -20,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     private AnalyzeFragment analyzeFragment = new AnalyzeFragment();
     private RecommendFragment recommendFragment = new RecommendFragment();
     private MypageFragment mypageFragment = new MypageFragment();
+    private PhotoActivity photoActivity = new PhotoActivity();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,4 +72,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
 }

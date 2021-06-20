e59654f409cda08838146f3c18f403819a2d1735
lcy8047 <lcy8047@gmail.com>
2021-06-20 07:21:15 +0000

    foodapi 음식정보가 있지만 입력한 음식이름과 다를 경우 추가 - 사용은 하지 않음

--- server/foodapi.py
+++ server/foodapi.py
@@ -22,20 +22,34 @@ class FoodApi:
     # 음식 정보가 하나도 없을 경우 -1
     # 음식 정보가 있으나 정확히 음식이름과 일치하지 않으면 0
     # 찾으면 칼로리 정보 리턴
-    def getCalorie(self, foodname: str) -> int:
+    def getCalorie(self, foodname: str) -> str:
         parameter = quote("DESC_KOR="+foodname)
         data = urlopen(self.url+parameter).read()
         data = json.loads(data)
         if(data[self.serviceid]['total_count'] == '0'):
-            return 0
+            return '0'
         for food in data[self.serviceid]['row']:
             if(food['DESC_KOR'] == foodname):
                 return food['NUTR_CONT1']
-        return 0
+        return '0'
+
+    # 음식정보가 있을 때 음식이름과 일치하지 않으면 그 음식정보를 이름과 함께 리턴
+    # 없으면 ('not found', 0) 리턴
+    # 있으면 음식이름과 칼로리 리턴
+    def getCalTest(self, foodname:str) -> tuple:
+        parameter = quote("DESC_KOR="+foodname)
+        data = urlopen(self.url+parameter).read()
+        data = json.loads(data)
+        if(data[self.serviceid]['total_count'] == '0'):
+            return ('not found', 0)
+        for food in data[self.serviceid]['row']:
+            if(food['DESC_KOR'] == foodname):
+                return (foodname,food['NUTR_CONT1'])
+        return (data[self.serviceid]['row'][0]['DESC_KOR'],data[self.serviceid]['row'][0]['NUTR_CONT1'])
 
 if __name__ == '__main__':
     api = FoodApi()
     print("api test")
-    print("get 가자미구이 calorie")
-    foodname = '가자미구이'
-    print("result : ", api.getCalorie(foodname))
+    foodname = input("input foodname : ")
+    print("get ",foodname," calorie")
+    print("result : ", api.getCalTest(foodname))

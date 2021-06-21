d15761be84255744e43c72ef2b668a569d645b52
lcy8047 <lcy8047@gmail.com>
2021-06-21 13:29:46 +0000

    서버 로그 시간체크 기능 추가

--- server/server.py
+++ server/server.py
@@ -16,27 +16,32 @@ import select
 '''
 etc..
 '''
-from time import sleep
+from time import gmtime, strftime
+import os
 
 # db초기화가 여러번 되면 문제가 생겨 한번만 객체 생성해서 사용함
 db = DB()
 fmodel = FoodModel()
 fapi = FoodApi()
 
+def getTime():
+    print(strftime("%Y-%m-%d %H:%M:%S", gmtime()), " : ", end='')
+
 def recvFile(client_socket, filename):
-    client_socket.settimeout(0.1)
-    with open(filename, 'wb') as f:
+    client_socket.settimeout(0.5)
+    with open(filename, 'wb', 0) as f:
         try:
             data = client_socket.recv(32768)
             #print("recv data: ", len(data))
             while data:
                 f.write(data)
-                sleep(0.01)
                 data = client_socket.recv(32768)
                 #print("recv data: ", len(data))
         except Exception as ex:
             print(ex)
-    f.close()
+        os.fsync(f)
+        f.close()
+    getTime()
     print("file closed")
 
 def makeSendData(foods):
@@ -52,36 +57,47 @@ def run(client_socket, addr):
     global fapi
 
     data = client_socket.recv(1024)
+    getTime()
     print("recv type:",data[:3])
     dataType = data[:3].decode('utf-8')
     if(dataType == "str"):
+        getTime()
         print("input type : string")
         result = client_socket.recv(1024)
+        getTime()
         print("recv foodname:", result)
         result = result.decode('utf-8')
         
         food = result
+        getTime()
         print("foodname : ", food)
         calorie = fapi.getCalorie(food)
-        print("send calorie: ", calorie)
+        getTime()
+        print("calorie get by api: ", calorie)
         client_socket.sendall(calorie.encode('utf-8'))
+        getTime()
         print("send:", calorie)
 
     elif(dataType == "img"):
+        getTime()
         print("input type : image")
         filename = "./recvfiles/"+"recvfile_"+str(addr[1])+".jpg"
     
         recvFile(client_socket, filename)
+        getTime()
         print("file recevied")
     
+        getTime()
         print("predict image")
         foods = fmodel.predict_image(filename)
         data = makeSendData(foods)
         client_socket.sendall(json.dumps(data).encode('utf-8'))
+        getTime()
         print("send: ", data)
     
     
     client_socket.close()
+    getTime()
     print("client close")
 
 
@@ -90,23 +106,28 @@ if __name__ == '__main__':
     HOST = '192.168.0.46'
     PORT = 24546
 
+    getTime()
     print("socket create...",end='')
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print("OK")
 
+    getTime()
     print("socket setting...",end='')
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     print("OK")
 
+    getTime()
     print("socket binding...",end='')
     server_socket.bind((HOST,PORT))
     print("OK")
 
+    getTime()
     print("socket listening...")
     server_socket.listen(5)
 
     while True:
         client_socket, addr = server_socket.accept()
+        getTime()
         print('connected by', addr)
         client = threading.Thread(target=run, args=(client_socket, addr))
         client.start()
